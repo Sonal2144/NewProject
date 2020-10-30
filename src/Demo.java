3c09f4d0b6461f27f1f793e8870f93837a718f95
Sonal <sonal.yenpure96@gmail.com>
2020-10-30 16:00:59 +0530

    Second Commit

--- src/Demo.java
+++ src/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 		System.out.println(x++);//10 (11)  
 		System.out.println(++x);//12  
 		System.out.println(x--);//12 (11)  
-		System.out.println(--x);//10  
+		System.out.println(--x);//10
+		System.out.println(x);
+		System.out.println("Hi");
 		}
 	}  
